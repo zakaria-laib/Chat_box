ae14a7d605bd617c3d2f4328ad69fbc9777c7474
Zakaria Laib <fz_laib@esi.dz>
2020-01-06 17:57:22 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+var dbUrl = 'mongodb+srv://user:user@learning-node-uxsnq.mongodb.net/test?retryWrites=true&w=majority'
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
+var messages = [
+    {name: 'Tim', message: 'Hi'},
+    {name: 'Jane', message: 'Hello'}
+]
+
+mongoose.Promise = Promise
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
+
+app.get('/messages', (req, res) =>{
+    Message.find({}, (err, messages) =>{
+        res.send(messages)
+    })
+})
+
+app.post('/messages', async (req, res) =>{
+    try {
+        var message = new Message(req.body)
+
+        var savedMessage = await message.save()
+
+        console.log('saved')
+
+        var censored = await Message.findOne({ message: 'badword' })
+
+        if (censored)
+            await Message.remove({ _id: censored.id })
+        else
+            io.emit('message', req.body)
+
+        res.sendStatus(200)
+    } catch (error) {
+        res.sendStatus(500)
+        return console.error(error)
+    }
+})
+
+io.on('connection',(socket) =>{
+    console.log('a user connected')
+} )
+
+mongoose.connect(dbUrl ,  { useUnifiedTopology: true }, (err, MongoClient) =>{
+    console.log('mongo conneced', err)
+})
+
+
+var server = http.listen(3000, () => {
+    console.log('server is listening on port', server.address().port)
+})
